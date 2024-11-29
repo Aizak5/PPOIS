6580688e220bb46f31917e10b69dec8652599a61
Aizak5 <144949092+Aizak5@users.noreply.github.com>
2024-11-29 15:58:29 +0300

    Update multiset_test.cpp

--- multiset_test.cpp
+++ multiset_test.cpp
@@ -2,32 +2,33 @@
 #include "Multiset.h"
 
 void testMultiset() {
-    std::cout << "Test 1: Creating and adding elements from std::string\n";
+    setlocale(LC_ALL,"rus");
+    std::cout << "Test 1: Создать и добавить элементы из std::string\n";
     Multiset set1 = Multiset::fromString("{a, b, d}");
     set1.print();
     std::cout << "\n";
 
-    std::cout << "Test 2: Creating and adding elements from char*\n";
+    std::cout << "Test 2: Создать и добавить элементы из char*\n";
     const char* str = "{d, e, f}";
     Multiset set2 = Multiset::fromString(str);
     set2.print();
     std::cout << "\n";
 
-    std::cout << "Test 3: Union of two sets\n";
+    std::cout << "Test 3: Объединение двух множеств\n";
     Multiset unionSet = set1 + set2;
-    std::cout << "Union of set1 and set2: ";
+    std::cout << "Объединение set1 и set2: ";
     unionSet.print();
     std::cout << "\n";
 
-    std::cout << "Test 4: Intersection of two sets\n";
+    std::cout << "Test 4: Пересечение двух множеств\n";
     Multiset intersectionSet = set1 * set2;
-    std::cout << "Intersection of set1 and set2: ";
+    std::cout << "Пересечение set1 и set2: ";
     intersectionSet.print();
     std::cout << "\n";
 
-    std::cout << "Test 5: Difference of two sets\n";
+    std::cout << "Test 5: Разность двух множеств\n";
     Multiset differenceSet = set1 - set2;
-    std::cout << "Difference of set1 and set2: ";
+    std::cout << "Разность set1 и set2: ";
     differenceSet.print();
     std::cout << "\n";
 }

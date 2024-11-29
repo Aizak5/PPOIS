5a124325e9dc2062ec9550f75797ca7bf490a070
Aizak5 <144949092+Aizak5@users.noreply.github.com>
2024-11-29 23:49:48 +0300

    Update multiset.h

--- multiset.h
+++ multiset.h
@@ -1,39 +1,40 @@
-#ifndef MULTISET_H
-#define MULTISET_H
-
-#include <iostream>
-#include <set>
+#pragma once
+#include <map>
 #include <string>
+#include <iostream>
 #include <sstream>
-#include <memory>
+#include <algorithm>
 
 class Multiset {
-private:
-    std::set<char> elements; 
-
 public:
     Multiset();
-    Multiset(const char& elem);
+    explicit Multiset(const char& elem);
 
     bool isEmpty() const;
+
     void add(const char elem);
     void remove(const char elem);
+
     size_t size() const;
+
     bool contains(const char elem) const;
-    
-    Multiset operator+(const Multiset& other) const;
-    void operator+=(const Multiset& other);
-    Multiset operator*(const Multiset& other) const;
-    void operator*=(const Multiset& other);
-    Multiset operator-(const Multiset& other) const;
-    void operator-=(const Multiset& other);
+
+    Multiset operator+(const Multiset& other) const;  
+    void operator+=(const Multiset& other);          
+
+    Multiset operator*(const Multiset& other) const;  
+    void operator*=(const Multiset& other);         
+
+    Multiset operator-(const Multiset& other) const;  
+    void operator-=(const Multiset& other);          
 
     static Multiset fromString(const std::string& str);
-    static Multiset fromString(const char* str); 
+    static Multiset fromString(const char* str);
+
     std::string toString() const;
 
     void print() const;
 
+private:
+    std::map<char, size_t> elements;
 };
-
-#endif 

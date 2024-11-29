8626983e60807671aee7b252076c00f600f0c9ea
Aizak5 <144949092+Aizak5@users.noreply.github.com>
2024-11-29 15:52:46 +0300

    Update multiset.h

--- multiset.h
+++ multiset.h
@@ -9,21 +9,18 @@
 
 class Multiset {
 private:
-    std::set<char> elements; // Множество элементов типа char
+    std::set<char> elements; 
 
 public:
-    // Конструкторы
     Multiset();
     Multiset(const char& elem);
 
-    // Методы
     bool isEmpty() const;
     void add(const char elem);
     void remove(const char elem);
     size_t size() const;
     bool contains(const char elem) const;
     
-    // Операторы
     Multiset operator+(const Multiset& other) const;
     void operator+=(const Multiset& other);
     Multiset operator*(const Multiset& other) const;
@@ -31,14 +28,12 @@ public:
     Multiset operator-(const Multiset& other) const;
     void operator-=(const Multiset& other);
 
-    // Методы для строк
     static Multiset fromString(const std::string& str);
-    static Multiset fromString(const char* str); // Перегрузка для char*
+    static Multiset fromString(const char* str); 
     std::string toString() const;
 
-    // Метод вывода множества
     void print() const;
 
 };
 
-#endif // MULTISET_H
+#endif 

19698aecd43312e884aa32b5f0610b28843909a4
Aizak5 <144949092+Aizak5@users.noreply.github.com>
2024-12-21 04:53:55 +0300

    Delete Deanery directory

--- Deanery/GradeBook.cpp
+++
@@ -1,9 +0,0 @@
-#include "gradebook.h"
-#include <iostream>
-
-Gradebook::Gradebook(int studentId)
-    : studentId(studentId) {}
-
-void Gradebook::addGrade(const std::string& courseName, const std::string& grade) {
-    grades.push_back({ courseName, grade });
-}
